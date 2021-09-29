db09bb2eb0f181dae7ebc1b6f6812acac952b32a
Anand <anandakkoomart@gmail.com>
2021-09-29 13:07:56 +0800

    Adam implementation

--- compression/src/main/java/ui/primeq/App.java
+++ compression/src/main/java/ui/primeq/App.java
@@ -7,8 +7,13 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-// import ui.primeq.optimizer.Adam;
-// import ui.primeq.optimizer.Optimizer;
+import ui.primeq.optimizer.Adam;
+import ui.primeq.optimizer.Function;
+
+import java.util.Optional;
+import java.util.Random;
+
+import ui.primeq.optimizer.Optimizer;
 
 import org.javatuples.Triplet;
 
@@ -16,33 +21,34 @@ public class App
 {
     public static void main( String[] args ) throws IOException
     {
-        System.out.println("Hello There!");
-        quantum_circuit_runner runner = new quantum_circuit_runner();
-        ArrayList<Double> list = new ArrayList<Double>();
-        list.add(5.0);
-        list.add(4.0);
-        list.add(1.0);
+        int maxiter = 10;
+        int numVars = 5;
+        Optional<Double> tol = Optional.of(1e-6);
+        Optional<Double> lr = Optional.of(1.0);
+        Optional<Double> beta1 = Optional.of(0.9);
+        Optional<Double> beta2 = Optional.of(0.99);
+        Optional<Double> noiseFactor = Optional.of(1e-10);
+        Optional<Double> eps = Optional.of(1e-10);
+        Optional<Boolean> amsgrad = Optional.of(false);
+     
+        Adam opt = new Adam(maxiter, tol, lr, beta1, beta2, noiseFactor, eps, amsgrad);
+       
 
-        list.add(5.0);
-        list.add(5.0);
-        list.add(5.0);
-        list.add(5.0);
-        list.add(5.0);
-        list.add(5.0);
-        list.add(5.0);
-        list.add(5.0);
-        list.add(5.0);
-        list.add(5.0);
-        list.add(5.0);
-        list.add(5.0);
-        list.add(5.0);
-        list.add(5.0);
+        Function f = new Function("equation");
+        ArrayList<Double> initialPoint = new ArrayList<Double>();
 
-        Triplet<List<Double>, Double, Double> Res = new Triplet<List<Double>, Double, Double>(new ArrayList<Double>(), 0.1, 0.2);
-        System.out.println(Res);
+        Random rand = new Random();
+        for(int i = 0; i < numVars; i++){
+            initialPoint.add(rand.nextDouble() * Math.PI);
+        }
+
+        int t = 0;
+
+        while(t < 10){
+            Triplet<List<Double>, Double, Double> res = opt.minimize(f, initialPoint);
+            System.out.println(res);
+            t++;
+        }
         
-        Map<String, Integer> m = runner.run(list);
-        System.out.println(m);
-        System.out.println(runner.get_gradients());
     }
 }
