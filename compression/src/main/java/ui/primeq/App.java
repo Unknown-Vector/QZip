391b47817c86039d9c3595d21ce751d35cc23ef4
Sam Yong Jek <sam.yong.jek.94@gmail.com>
2021-10-25 15:05:17 +0800

    added user input for compress and decompress

--- compression/src/main/java/ui/primeq/App.java
+++ compression/src/main/java/ui/primeq/App.java
@@ -1,6 +1,9 @@
 package ui.primeq;
 
 import java.io.IOException;
+import java.util.Scanner;
+
+import org.codehaus.plexus.components.interactivity.Prompter;
 
 import ui.primeq.config.Config;
 import ui.primeq.optimizer.Adam;
@@ -9,6 +12,17 @@ import ui.primeq.storage.Storage;
 
 public class App {
     public static void main( String[] args ) throws IOException {
+
+        System.out.println("1. Compress or 2. Decompress");
+
+        Scanner scanner = new Scanner(System.in);
+        String command = scanner.nextLine().strip().toLowerCase();
+        while (!command.equals("compress") && !command.equals("decompress")) {
+            System.out.println("Please key in correct command.");
+            System.out.println("Command Entered: " + command);
+            command = scanner.nextLine().strip().toLowerCase();
+        }
+        scanner.close();
 
         // Initialize Config
         Config config = new Config();
@@ -31,22 +45,24 @@ public class App {
         // Convert File into int array to be processed
         storage.readData(nameOfFile, fileFormat);
 
-        // Process Unique Values with Optimizer
-        opt.processUniqueValues(config, functionManager, storage);
+        if (command.equals("compress")) {
+            // Process Unique Values with Optimizer
+            opt.processUniqueValues(config, functionManager, storage);
 
-        System.out.println("filename: " + storage.getFileName());
-        for (int remainder : storage.getRemainders()) {
-            System.out.println("remainder: " + remainder);
+            System.out.println("filename: " + storage.getFileName());
+            for (int remainder : storage.getRemainders()) {
+                System.out.println("remainder: " + remainder);
+            }
+
+            for (int key : storage.getUniqueMap().keySet()) {
+                System.out.println(key + ": " + storage.getUniqueMap().get(key));
+            }
+
+            // Generate Compressed file with unique map from Optimizer.
+            fileManager.generateCompressedFile(config, storage);
+        } else if (command.equals("decompress")) {
+            // Decompression of fill.
+            fileManager.decompress(nameOfFile + "Compressed.txt");
         }
-
-        for (int key : storage.getUniqueMap().keySet()) {
-            System.out.println(key + ": " + storage.getUniqueMap().get(key));
-        }
-
-        // Generate Compressed file with unique map from Optimizer.
-        fileManager.generateCompressedFile(config, storage);
-
-        // Decompression of fill.
-        fileManager.decompress(nameOfFile + "Compressed.txt");
     }
 }
